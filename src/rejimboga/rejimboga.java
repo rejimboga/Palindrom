ccc1a29b0b68525a54facd4f30cdbf329f60f17a
PussyDestrojer <pashabagan973@gmail.com>
2020-04-21 15:02:43 +0300

    123321

--- src/rejimboga/rejimboga.java
+++ src/rejimboga/rejimboga.java
@@ -7,10 +7,10 @@ public class rejimboga {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Введіть шестизначне число:");
+        System.out.print("Введіть число:");
         int a = sc.nextInt();
 
-        if(9 < a && 10000000 >= a){
+        if(999 < a && 10000000 > a){
             int b = (a/100000);
             int c = ((a%100000)/10000);
             int d = (((a%100000)%10000)/1000);
@@ -20,7 +20,9 @@ public class rejimboga {
 
             if (b == g && c == f && d == e){
                 System.out.println("Данне число є паліндромом");
-            }else
+            }else if(99 < a && 10000 > a){
+                System.out.println("Данне число є паліндромом");
+            } else
                 System.out.println("Данне число не є паліндромом");
         }
     }
